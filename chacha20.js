dc0a0ac3f4ea3a6a02bcaf4dfc3b4fc5e3c0a275
Olaf Bernstein <camel-cdr@protonmail.com>
2023-07-31 22:08:34 +0200

    added chacha20&poly1305 mangopi results

---
+++ chacha20.js
@@ -0,0 +1,14 @@
+let prefix = `
+<p>Implements the chacha20 ciphers.</p>
+
+<ul>
+	<li><b>rvv:</b> rvv implementation (thanks to <a href="https://github.com/edre/rvv-chacha-poly">https://github.com/edre/rvv-chacha-poly</a>)</li>
+	<li><b>boring:</b> SWAR implementation (from <a href="https://github.com/google/boringssl">boring SSL</a>)</li>
+</ul>
+
+<h3>Navigation:</h3>
+<ul>
+	<li><a href="index.html">Return to parent page</a></li>
+	<li><a href="https://github.com/edre/rvv-chacha-poly/blob/master/vchacha.s">View source code</a></li>
+</ul>
+`;
