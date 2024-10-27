882f38fb5b639ffffb4c09d9d0bade1f2e3bcd09
Olaf Bernstein <camel-cdr@protonmail.com>
2024-10-28 00:06:40 +0100

    add X60 veclibm

---
+++ veclibm.js
@@ -0,0 +1,12 @@
+let prefix = `
+<p>Measurements of the <a href="https://github.com/rivosinc/veclibm">rivos veclibm RVV math library</a> compared to libc. All input values are uniformly sampled from [0,1).</p>
+
+
+<h3>Navigation:</h3>
+<ul>
+	<li><a href="index.html">Return to parent page</a></li>
+	<li><a href="https://github.com/camel-cdr/rvv-bench/blob/main/single/veclibm.S">View source code</a></li>
+</ul>
+`;
+
+document.getElementById("main").insertAdjacentHTML('beforeend', prefix);
