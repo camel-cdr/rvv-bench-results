99b37d57dd92fc1d564ac3b86d2b5958d4b1116a
Olaf Bernstein <camel-cdr@protonmail.com>
2025-07-08 00:21:53 +0200

    fix typo

--- templates/trans8x8e16.js
+++ templates/trans8x8e16.js
@@ -17,6 +17,6 @@ let prefix = `
 <h3>Navigation:</h3>
 <ul>
 	<li><a href="index.html">Return to parent page</a></li>
-	<li><a href="https://github.com/camel-cdr/rvv-bench/blob/main/bench/trans8x8e8.S">View source code</a></li>
+	<li><a href="https://github.com/camel-cdr/rvv-bench/blob/main/bench/trans8x8e16.S">View source code</a></li>
 </ul>
 `;
