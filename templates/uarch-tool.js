350b5f22e4161213a2bb0fcc0874b343c7a091ab
Olaf Bernstein <camel-cdr@protonmail.com>
2025-09-12 06:41:23 +0200

    update latest rvv-bench changes for bpi_f3

---
+++ templates/uarch-tool.js
@@ -0,0 +1,12 @@
+let prefix = `
+<p>A small tool that tries to measure specific uarch properties.</p>
+
+
+<h3>Navigation:</h3>
+<ul>
+	<li><a href="index.html">Return to parent page</a></li>
+	<li><a href="https://github.com/camel-cdr/rvv-bench/blob/main/single/uarch.c">View source code</a></li>
+</ul>
+`;
+
+document.getElementById("main").insertAdjacentHTML('beforeend', prefix);
